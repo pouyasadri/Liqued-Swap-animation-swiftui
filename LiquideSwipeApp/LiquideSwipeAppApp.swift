c7b491f2925cbd577ec5280e925b843b66fed7bc
pouyasadri <pouyapaghandeh78@gmail.com>
2024-01-23 21:23:31 +0100

    Initial Commit

---
+++ LiquideSwipeApp/LiquideSwipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiquideSwipeAppApp.swift
+//  LiquideSwipeApp
+//
+//  Created by Pouya Sadri on 23/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct LiquideSwipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
